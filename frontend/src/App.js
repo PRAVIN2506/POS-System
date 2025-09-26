1bce4eec31386f9fa55af095fa46c20dc62cc158
PRAVIN2506 <pravin250604@gmail.com>
2025-09-26 12:49:15 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/HomePage";
 import POSPage from "./pages/POSPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/pos" element={<POSPage/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
